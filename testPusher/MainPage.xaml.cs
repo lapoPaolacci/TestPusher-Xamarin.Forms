5e70d9de71bd9c954cbd2f650525a0b7e90c029c
Lapo Paolacci <lapo.paolacci@madeinapp.net>
2021-09-21 18:38:30 +0200

    first commit

---
+++ testPusher/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using PusherClient;
+using Xamarin.Forms;
+
+namespace testPusher
+{
+    public partial class MainPage : ContentPage
+    {
+        Pusher pusher;
+
+        //CONFIGURATION
+        string applicationKey = "APPLICATION_KEY";
+        string cluster = "CLUSTER eg. eu, us";
+        string channelName = "CHANNEL_NAME";
+        string eventName = "EVENT_NAME";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Setup();
+        }
+
+        public async void Setup()
+        {
+            pusher = new Pusher(applicationKey, new PusherOptions()
+            {
+                Cluster = cluster,
+                Encrypted = true
+            });
+
+            pusher.ConnectionStateChanged += Pusher_ConnectionStateChanged;
+            pusher.Error += Pusher_Error;
+            await pusher.ConnectAsync();
+
+            var channel = await pusher.SubscribeAsync(channelName);
+            channel.Bind(eventName, (dynamic data) =>
+            {
+                Console.WriteLine(data);
+            });
+
+        }
+
+        private void Pusher_Error(object sender, PusherException error)
+        {
+            Console.WriteLine(error);
+        }
+
+        private void Pusher_ConnectionStateChanged(object sender, ConnectionState state)
+        {
+            Console.WriteLine(state);
+        }
+    }
+}
